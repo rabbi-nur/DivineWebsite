dc07ece1a55f820e7a66e7d3e9cda43eeda20ec9
Shadman Sakib <shadmanabir@outlook.com>
2025-07-17 16:53:51 +0600

    refactored

--- pages/deploy.php
+++ pages/deploy.php
@@ -7,3 +7,7 @@ $output = shell_exec('git pull 2>&1');
 
 echo "<pre>$output</pre>";
 ?>
+
+<?php
+// If there are any include('header.php'), include('footer.php'), or include('head.php'), update to include('includes/header.php'), etc.
+?>
