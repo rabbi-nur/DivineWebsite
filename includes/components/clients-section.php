24a1efa0d347e3d8ebe222202fe781add6f6fb07
Rabbinur <140503888+rabbi-nur@users.noreply.github.com>
2025-07-22 19:05:18 +0600

    Update client logos section and adjust background color in styles

--- includes/components/clients-section.php
+++ includes/components/clients-section.php
@@ -7,9 +7,9 @@ $client_logos = $client_logos ?? [
     'img/logo-5.png', 'img/logo-6.png', 'img/logo-7.png', 'img/logo-8.png'
 ];
 ?>
-<section class="container-xxl client-logos py-5 buyer container-fluid buyer-bg">
+<section class="container-xxl client-logos py-5 buyer container-fluid ">
     <?php render_section_title('Our Clients', 'Driving Impact Through Responsible Innovation'); ?>
-    <div class="logo-grid">
+    <div class="logo-grid ">
         <?php foreach($client_logos as $index => $logo): ?>
         <img src="<?= $logo ?>" alt="Client Logo <?= $index + 1 ?>" class="logo fade-in" width="200px">
         <?php endforeach; ?>
